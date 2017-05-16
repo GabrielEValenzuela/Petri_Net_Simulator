0e53bff2b0da91ede473e9402b01a2255cfcac7d
joaquinfelici <joaquinfelici@gmail.com>
2017-05-16 19:58:18 -0300

    Fixed about.hmtl reading (now resouce instead of file).

--- src/org/petrinator/editor/actions/AboutAction.java
+++ src/org/petrinator/editor/actions/AboutAction.java
@@ -20,14 +20,11 @@ package org.petrinator.editor.actions;
 
 import java.awt.*;
 import java.awt.event.ActionEvent;
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.util.Scanner;
 import javax.swing.*;
-
 import org.petrinator.util.GraphicsTools;
 import org.petrinator.editor.Root;
-import pipe.gui.widgets.ButtonBar;
 import pipe.gui.widgets.EscapableDialog;
 import org.petrinator.auxiliar.ResultsHTMLPane;
 
@@ -49,7 +46,9 @@ public class AboutAction extends AbstractAction {
 
     public void actionPerformed(ActionEvent e)
     {
-
+        /*
+         * Create the dialog
+         */
         EscapableDialog guiDialog = new EscapableDialog(root.getParentFrame(), "About", false);
         Container contentPane = guiDialog.getContentPane();
         ResultsHTMLPane results = new ResultsHTMLPane("");
@@ -58,49 +57,18 @@ public class AboutAction extends AbstractAction {
         guiDialog.setLocationRelativeTo(root.getParentFrame());
         guiDialog.setVisible(true);
 
+        /*
+         * Read the about.html file
+         */
+        InputStream aboutFile = getClass().getResourceAsStream("/resources/about.html");
         Scanner scanner = null;
-        try
-        {
-            scanner = new Scanner(new File("src/resources/about.html"), "UTF-8");
-        } catch (FileNotFoundException e1)
-        {
-            e1.printStackTrace();
-        }
+        scanner = new Scanner(aboutFile, "UTF-8");
         String s = scanner.useDelimiter("\\Z").next();
         scanner.close();
-        results.setText(s);
 
         /*
-        JOptionPane.showOptionDialog(
-                root.getParentFrame(),  "                                                      " + "Universidad Nacional de Cordoba, Argentina\n" +
-                root.getAppLongName() + "\n" 
-                + "\n"
-                + "Authors: \nJoaquin Rodriguez Felici\n	Leandro Asson \n\n"
-                + "Contributors:\n"
-                + "Martin Riesz (PNEditor)\n"
-                + "Ján Tančibok (inhibitor arcs)\n"
-                + "Maxim Gready, James Bloom (Classification) \n"
-                + "Nadeem Akharware (Invariants) \n"
-                + "Matthew Worthington, Edwin Chung, Will Master (Reachability) \n"
-                + "Pere Bonet (Siphons) \n"
-                + "\n"
-                + "This program is free software:  you can redistribute it and/or modify          \n"
-                + "it under the terms of the GNU General Public License as published by\n"
-                + "the Free Software Foundation, either version 3 of the License, or any\n"
-                + "later version.\n"
-                + "\n"
-                + "This program is distributed in the hope that it will be useful, but with \n"
-                + "no warranty or fitness for a particular purpose. See the GNU General \n"
-                + "Public License for more details.\n\n"
-                + "You should have received a copy of the GNU General Public License\n"
-                + "along with this program. If not, see <http://www.gnu.org/licenses/>. \n\n",
-                "About",
-                JOptionPane.DEFAULT_OPTION,
-                JOptionPane.INFORMATION_MESSAGE,
-                GraphicsTools.getIcon("icon48.png"),
-                new String[]{"OK"},
-                "OK");
-                */
+         * Show the text on dialog
+         */
+        results.setText(s);
     }
-
 }
